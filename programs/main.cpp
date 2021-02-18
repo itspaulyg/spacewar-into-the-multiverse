72ecb721d9f36cf000db7101da82cd9a89d10cbb
itspaulyg <pgierl@nd.edu>
2021-02-18 00:13:19 -0500

    added second spaceship, comments show what needs to be added

--- programs/main.cpp
+++ programs/main.cpp
@@ -1,22 +1,25 @@
 #include "../classes/spaceship.h"
 
-void pollEvents(Window &window, Spaceship &spaceship) {
+void pollEvents(Window &window, Spaceship &spaceship1) {
 	SDL_Event event;
 
+// need to add movement and events for spaceship2
 	if(SDL_PollEvent(&event)) {
-		spaceship.pollEvents(event);
+		spaceship1.pollEvents(event);
 		window.pollEvents(event);
 	}
 }
 
 int main() {
 
-	Window window("Tutorial", 800, 600);
-	Spaceship spaceship(window, 10, 10, 100, 100, "rocketship.png");
+	Window window("Spacewar! Into the Multiverse", 1200, 900);
+	Spaceship spaceship1(window, 30, 45, 100, 100, 0, 0, 0, 0, "rocketship.png");
+	Spaceship spaceship2(window, 20, 50, 500, 500, 0, 0, 0, 0, "verticalrocket.png");
 
 	while(!window.isClosed()) {
-		pollEvents(window, spaceship);
-		spaceship.draw();
+		pollEvents(window, spaceship1);
+		spaceship1.draw();
+		spaceship2.draw();
 		window.clear();
 	}
 
--- programs/spaceship.cpp
+++ programs/spaceship.cpp
@@ -1,7 +1,8 @@
 #include "../classes/spaceship.h"
 
+void check_boundaries(int, int);
 
-Spaceship::Spaceship(const Window &win, int wIn, int hIn, int xIn, int yIn, const std::string &image_path) : Window(win), w(wIn), h(hIn), x(xIn), y(yIn)
+Spaceship::Spaceship(const Window &win, int wIn, int hIn, int xIn, int yIn, int xVelIn, int yVelIn, int wVelIn, double deg, const std::string &image_path) : Window(win), w(wIn), h(hIn), x(xIn), y(yIn), xVel(xVelIn), yVel(yVelIn), wVel(wVelIn), degree(deg)
 {
 	SDL_Surface *surface = IMG_Load(image_path.c_str());
 	if(!surface) {
@@ -28,27 +29,62 @@ void Spaceship::draw() const {
 	SDL_Rect spaceship = {x, y, w, h};
 
 	if(texture) {
-		SDL_RenderCopy(renderer, texture, nullptr, &spaceship);
+		SDL_RenderCopyEx(renderer, texture, nullptr, &spaceship, degree, NULL, SDL_FLIP_NONE);
 	}
 }
 
 void Spaceship::pollEvents(SDL_Event &event) {
+
+//need to add acceleration on keydown
 	if(event.type == SDL_KEYDOWN) {
 		switch(event.key.keysym.sym) {
 			case SDLK_LEFT:
-				x -= 10;
+				degree -= 10;
 				break;
 			case SDLK_RIGHT:
-				x += 10;
+				degree += 10;
 				break;
 			case SDLK_UP:
-				y -= 10;
+				x += int(10*sin(degree * PI / 180));
+				y -= int(10*cos(degree * PI / 180));
+				check_boundaries(x, y);
 				break;
 			case SDLK_DOWN:
-				y += 10;
+				x = rand()%500 + 50;
+				y = rand()%500 + 50;
+//				SDL_Delay(3000); not sure if this can be used for hyperspace delay
 				break;
 			default:
 				break;
 		}
+	}
+
+// need to add deceleration on keyup
+	if(event.type == SDL_KEYUP) {
+		switch(event.key.keysym.sym) {
+			case SDLK_UP:
+				x += int(10*sin(degree * PI / 180));
+				y -= int(10*cos(degree * PI / 180));
+				check_boundaries(x, y);
+				break;
+			default:
+				break;
+		}
+	}
+}
+
+// need to determine how to check boundaries correctly
+void check_boundaries(int x, int y) {
+	if(y >= 1080) {
+		y = 0;
+	}
+	else if(y <= 0) {
+		y = 1080;
+	}
+	if(x >= 920) {
+		x = 0;
+	}
+	else if(x <= 0) {
+		x = 920;
 	}
 }
